f5bdea65645ff216e10b48a666e2c6826d49579c
Ekkana <alexeykerpel15@gmail.com>
2025-03-15 13:27:32 -0400

    Moved diffview

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,3 +1,12 @@
+vim.api.nvim_create_user_command("DiffviewFileHistoryCurrent", function()
+    local filepath = vim.fn.expand("%:p") -- Get the full path of the current buffer
+    if filepath == "" then
+        print("No file to show history for")
+        return
+    end
+    vim.cmd("DiffviewFileHistory " .. filepath)
+end, {})
+
 return {
     "NeogitOrg/neogit",
     dependencies = {
@@ -7,5 +16,15 @@ return {
         -- Only one of these is needed.
         "nvim-telescope/telescope.nvim", -- optional
     },
-    config = true,
+    config = function()
+        vim.keymap.set("n", "<leader>n", ":Neogit<CR>", { desc = "[N]eogit" })
+        vim.keymap.set("n", "<leader>do", ":DiffviewOpen<CR>", { desc = "[D]iff [O]pen" })
+        vim.keymap.set("n", "<leader>dc", ":DiffviewClose<CR>", { desc = "[D]iff [C]lose" })
+        vim.keymap.set(
+            "n",
+            "<leader>df",
+            ":DiffviewFileHistoryCurrent<CR>",
+            { desc = "[D]iff [F]iles" }
+        )
+    end,
 }
